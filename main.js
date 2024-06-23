875e31155c3f983278c81a33b92d461f237128c9
Dario Jularic <dariojularic0@gmail.com>
2024-06-23 23:41:58 +0200

    Edit class

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 import './style.css'
 
 class User {
-  constructor(firstName, lastName, img, address, posts, comments, likes) {
+  constructor(firstName, lastName, img, address, friends, posts, comments, likes) {
     this.firstName = firstName
     this.lastName = lastName
     this.img = img
     this.address = address
+    this.friends = friends
     this.posts = posts
     this.comments = comments
     this.likes = likes
