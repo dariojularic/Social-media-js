981b81c0812f205facd6229acbac659a81a7da3f
Dario Jularic <dariojularic0@gmail.com>
2024-06-23 23:14:16 +0200

    Create user class

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 import './style.css'
 
 class User {
-  constructor(firstName, lastName, posts, comments, likes) {
+  constructor(firstName, lastName, img, address, posts, comments, likes) {
     this.firstName = firstName
+    this.lastName = lastName
+    this.img = img
+    this.address = address
     this.posts = posts
     this.comments = comments
     this.likes = likes
+  }
+}
+
+class userManager {
+  constructor() {
+    
   }
 }
